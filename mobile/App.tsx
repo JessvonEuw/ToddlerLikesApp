c62daf7cbe67cdfb59d590a31d9460c6731a38c9
Jess von Euw <jessvoneuw@gmail.com>
2025-02-06 08:49:18 -0700

    Initial expo router setup

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello world hi there it is me</Text>
+      <Text>Hello world</Text>
       <StatusBar style="auto" />
     </View>
   );
---
+++ mobile/app/items.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function ItemScreen() {
+  return (
+    <View>
+      <Text style={{ fontSize: 30 }}>Item screen</Text>
+    </View>
+  );
+}
